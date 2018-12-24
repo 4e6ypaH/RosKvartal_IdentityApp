ad087f75a81be6e52e20e12dde456041a09cec31
Maxim Troshkov <maxitm_troshkov@mail.ru>
2018-12-24 21:54:19 +0300

    lesson 60

--- IdentityApp/Controllers/HomeController.cs
+++ IdentityApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IdentityApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,17 @@ namespace IdentityApp.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetUsers()
+        {
+            List<ApplicationUser> users = new List<ApplicationUser>();
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                users = db.Users.ToList();
+            }
+
+            return View(users);
         }
 
         public ActionResult About()
